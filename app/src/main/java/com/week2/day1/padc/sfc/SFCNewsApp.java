2acd1b9b400b205d6f7c174a6feb2e9a6bc2a7fe
Min Thein Win <mmtw.cs@gmail.com>
2017-11-04 17:48:54 +0630

    [add] initialize skeleton projects with default structure /value.

---
+++ app/src/main/java/com/week2/day1/padc/sfc/SFCNewsApp.java
@@ -0,0 +1,16 @@
+package com.week2.day1.padc.sfc;
+
+import android.app.Application;
+
+/**
+ * Created by Min Thein Win on 11/4/2017.
+ */
+
+public class SFCNewsApp extends Application{
+    public static final String LOG_TAG="SFCNewsApp";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
